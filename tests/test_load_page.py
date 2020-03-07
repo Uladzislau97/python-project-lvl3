758f24b010ae3d854378ae040405d03389d7f876
Uladzislau Vasilyeu <VSvasilyev97@gmail.com>
2020-03-07 20:09:36 +0300

    Add tests for loading page with local resources

--- tests/test_load_page.py
+++ tests/test_load_page.py
@@ -8,11 +8,9 @@ from page_loader import load_page
 
 
 def test_load_page():
-    address = 'https://hexlet.io/courses'
-    response_text = '<div>test</div>'
-    temp_dir = tempfile.TemporaryDirectory()
-
     with requests_mock.mock() as m:
+        address = 'https://hexlet.io/courses'
+        response_text = '<div>test</div>'
         m.get(address, text=response_text)
 
         with tempfile.TemporaryDirectory() as tmpdirname:
@@ -21,3 +19,55 @@ def test_load_page():
 
             with open(result_path, 'r') as f:
                 assert f.read() == response_text
+
+
+def test_load_page_with_local_resources():
+    with requests_mock.mock() as m:
+        host = 'https://hexlet.io'
+        address = host + '/courses'
+
+        with open('tests/fixtures/index.html', 'r') as f:
+            m.get(address, text=f.read())
+        
+        with open('tests/fixtures/index.css', 'r') as f:
+            request_address = host + '/assets/index.css'
+            result_css_content = f.read()
+            m.get(request_address, text=result_css_content)
+        
+        with open('tests/fixtures/index.js', 'r') as f:
+            request_address = host + '/assets/index.js'
+            result_js_content = f.read()
+            m.get(request_address, text=result_js_content)
+        
+        with open('tests/fixtures/index.jpg', 'rb') as f:
+            request_address = host + '/assets/index.jpg'
+            result_bin_content = f.read()
+            m.get(request_address, content=result_bin_content)
+        
+        with open('tests/fixtures/result.html', 'r') as f:
+            result_html_content = f.read()
+        
+        with tempfile.TemporaryDirectory() as tmpdirname:
+            load_page(address, tmpdirname)
+
+            result_html_path = os.path.join(tmpdirname, 'hexlet-io-courses.html')
+            with open(result_html_path, 'r') as f:
+                assert f.read() == result_html_content
+            
+            result_css_path = os.path.join(
+                tmpdirname, 'hexlet-io-courses_files/assets-index.css'
+            )
+            with open(result_css_path, 'r') as f:
+                assert f.read() == result_css_content
+            
+            result_js_path = os.path.join(
+                tmpdirname, 'hexlet-io-courses_files/assets-index.js'
+            )
+            with open(result_js_path, 'r') as f:
+                assert f.read() == result_js_content
+            
+            result_bin_path = os.path.join(
+                tmpdirname, 'hexlet-io-courses_files/assets-index.jpg'
+            )
+            with open(result_bin_path, 'rb') as f:
+                assert f.read() == result_bin_content

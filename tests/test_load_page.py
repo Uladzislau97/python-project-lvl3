3a9c8b09553f4521d3e1c47a03468b060c1834fb
Uladzislau Vasilyeu <VSvasilyev97@gmail.com>
2020-03-15 09:34:47 +0300

    Update tests

--- tests/test_load_page.py
+++ tests/test_load_page.py
@@ -1,4 +1,3 @@
-import requests
 import tempfile
 import os
 
@@ -10,7 +9,7 @@ from page_loader import load_page
 def test_load_page():
     with requests_mock.mock() as m:
         address = 'https://hexlet.io/courses'
-        response_text = '<div>test</div>'
+        response_text = '<div>\n test\n</div>'
         m.get(address, text=response_text)
 
         with tempfile.TemporaryDirectory() as tmpdirname:
@@ -28,44 +27,44 @@ def test_load_page_with_local_resources():
 
         with open('tests/fixtures/index.html', 'r') as f:
             m.get(address, text=f.read())
-        
+
         with open('tests/fixtures/index.css', 'r') as f:
             request_address = host + '/assets/index.css'
             result_css_content = f.read()
             m.get(request_address, text=result_css_content)
-        
+
         with open('tests/fixtures/index.js', 'r') as f:
             request_address = host + '/assets/index.js'
             result_js_content = f.read()
             m.get(request_address, text=result_js_content)
-        
+
         with open('tests/fixtures/index.jpg', 'rb') as f:
             request_address = host + '/assets/index.jpg'
             result_bin_content = f.read()
             m.get(request_address, content=result_bin_content)
-        
+
         with open('tests/fixtures/result.html', 'r') as f:
             result_html_content = f.read()
-        
+
         with tempfile.TemporaryDirectory() as tmpdirname:
             load_page(address, tmpdirname)
 
             result_html_path = os.path.join(tmpdirname, 'hexlet-io-courses.html')
             with open(result_html_path, 'r') as f:
                 assert f.read() == result_html_content
-            
+
             result_css_path = os.path.join(
                 tmpdirname, 'hexlet-io-courses_files/assets-index.css'
             )
             with open(result_css_path, 'r') as f:
                 assert f.read() == result_css_content
-            
+
             result_js_path = os.path.join(
                 tmpdirname, 'hexlet-io-courses_files/assets-index.js'
             )
             with open(result_js_path, 'r') as f:
                 assert f.read() == result_js_content
-            
+
             result_bin_path = os.path.join(
                 tmpdirname, 'hexlet-io-courses_files/assets-index.jpg'
             )

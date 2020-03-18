09ffd5732060b681fb2656918d3a6ef448528e15
Uladzislau Vasilyeu <VSvasilyev97@gmail.com>
2020-03-18 21:40:04 +0300

    Add cli logging level selection

--- page_loader/scripts/page_loader.py
+++ page_loader/scripts/page_loader.py
@@ -1,9 +1,39 @@
 #!/usr/bin/env python3
 import os
+import logging
 
 import argparse
 
 from page_loader import load_page
+
+
+LOGGING_LEVEL_NAMES = (
+    NOTSET, DEBUG, INFO, WARNING, ERROR, CRITICAL
+) = (
+    'notset', 'debug', 'info', 'warning', 'error', 'critical'
+)
+
+
+logging_levels = {
+    NOTSET: logging.NOTSET,
+    DEBUG: logging.DEBUG,
+    INFO: logging.INFO,
+    WARNING: logging.WARNING,
+    ERROR: logging.ERROR,
+    CRITICAL: logging.CRITICAL
+}
+
+
+def logging_level(level_name):
+    try:
+        return logging_levels[level_name]
+    except ValueError:
+        raise argparse.ArgumentTypeError(
+            'Unknown logging level: "{}". Use one of this: {}'.format(
+                level_name,
+                ', '.join(LOGGING_LEVEL_NAMES),
+            )
+        )
 
 
 def main():
@@ -19,8 +49,14 @@ def main():
         type=str,
         default=os.getcwd()
     )
+    parser.add_argument(
+        '-l', '--logging',
+        help='set logging level',
+        type=logging_level,
+        default=WARNING
+    )
     args = parser.parse_args()
-    load_page(args.address, args.output)
+    load_page(args.address, args.output, args.logging)
 
 
 if __name__ == "__main__":
